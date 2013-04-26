fe6a6d01eb2bb1010a41dbe519b3b1f17a8535e6
Frank Junior <frankcbjunior@gmail.com>
2013-04-26 13:00:06 -0300

    added image support

--- src/gui/TelaPrincipal.java
+++ src/gui/TelaPrincipal.java
@@ -2,37 +2,45 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Insets;
-import java.awt.List;
-import java.awt.ScrollPane;
+import java.awt.RenderingHints;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
-import javax.swing.SpringLayout;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.plaf.InsetsUIResource;
-
-import com.sun.xml.internal.ws.org.objectweb.asm.Label;
+import javax.swing.plaf.basic.BasicBorders.RadioButtonBorder;
 
 public class TelaPrincipal extends JFrame {
 	
 	private GridBagConstraints gbc;
+	private JPanel painelDireita;
 	
 	public TelaPrincipal() {
 		gbc = new GridBagConstraints();
@@ -41,7 +49,7 @@ public class TelaPrincipal extends JFrame {
 	
 	private void initframe(){
 		//centralizando o Frame
-		this.setSize(800, 600);
+		this.setSize(900, 600);
 		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
 	    int x = (int) (((dimension.getWidth() / 2) - (this.getWidth() / 2)));
 	    int y = (int) (((dimension.getHeight() / 2) - (this.getHeight() / 2)));
@@ -92,41 +100,45 @@ public class TelaPrincipal extends JFrame {
 	      painelCentral.add(painelNorte, BorderLayout.NORTH);
 	      
 	      JPanel painelEsquerda = new JPanel();
-	      painelEsquerda.setLayout(new GridBagLayout());
+	      painelEsquerda.setLayout(new BorderLayout());
+	      
+	      JPanel painelTagsGerais = new JPanel();
+	      painelTagsGerais.setLayout(new GridBagLayout());
 	      gbc.gridx = 0;
 	      gbc.gridy = 0;
 	      gbc.anchor = GridBagConstraints.WEST;
 	      gbc.insets = new Insets(10, 10, 0, 10);
-	      painelEsquerda.add(labelArtista,gbc);
+	      painelTagsGerais.add(labelArtista,gbc);
 	      gbc.gridx = 1;
 	      gbc.gridy = 0;
-	      painelEsquerda.add(TextFieldArtista,gbc);
+	      painelTagsGerais.add(TextFieldArtista,gbc);
 	      gbc.gridx = 0;
 	      gbc.gridy = 1;
-	      painelEsquerda.add(labelAlbum,gbc);
+	      painelTagsGerais.add(labelAlbum,gbc);
 	      gbc.gridx = 1;
 	      gbc.gridy = 1;
-	      painelEsquerda.add(TextFieldAlbum,gbc);
+	      painelTagsGerais.add(TextFieldAlbum,gbc);
 	      gbc.gridx = 0;
 	      gbc.gridy = 2;
-	      painelEsquerda.add(labelAno,gbc);
+	      painelTagsGerais.add(labelAno,gbc);
 	      gbc.gridx = 1;
 	      gbc.gridy = 2;
-	      painelEsquerda.add(TextFieldAno,gbc);
+	      painelTagsGerais.add(TextFieldAno,gbc);
 	      gbc.gridx = 0;
 	      gbc.gridy = 3;
 	      gbc.anchor = GridBagConstraints.NORTH;
-	      painelEsquerda.add(labelGenero,gbc);
+	      painelTagsGerais.add(labelGenero,gbc);
 	      gbc.gridx = 1;
 	      gbc.gridy = 3;
 	      gbc.weighty = 1;
 	      gbc.weightx = 1;
 	      gbc.anchor = GridBagConstraints.NORTHWEST;
-	      painelEsquerda.add(TextFieldGenero,gbc);
+	      painelTagsGerais.add(TextFieldGenero,gbc);
+	      painelEsquerda.add(painelTagsGerais, BorderLayout.CENTER);
 	      painelNorte.add(painelEsquerda);
 	      
-	      JPanel painelDireita = new JPanel();
-	      painelDireita.setLayout(null);
+	      painelDireita = new JPanel();
+	      painelDireita.setLayout(new BorderLayout());
 	      painelNorte.add(painelDireita);
 	      
 	      JPanel painelSul = new JPanel();
@@ -134,19 +146,98 @@ public class TelaPrincipal extends JFrame {
 	      painelCentral.add(painelSul,BorderLayout.CENTER);
 	      
 	      JPanel painelFaixas = new JPanel();
+	      painelFaixas.setBorder(BorderFactory.createTitledBorder("Musicas"));
 	      painelFaixas.setLayout(new GridBagLayout());
 	      painelSul.add(painelFaixas, BorderLayout.NORTH);
 	      
+	      JPanel painelBotoes = new JPanel();
+//	      painelBotoes.setBackground(new Color(0,255,0));
+	      painelBotoes.setLayout(new GridBagLayout());
+	      JButton botaoSalvar = new JButton("Salvar");
+	      JButton botaoNome2Tag = new JButton("Nome > Tag");
+	      gbc.gridx = 0;
+	      gbc.gridy = 0;
+	      gbc.weightx = 0;
+	      gbc.weighty = 0;
+	      gbc.anchor = GridBagConstraints.NORTHEAST;
+	      painelBotoes.add(botaoSalvar,gbc);
+	      gbc.gridx = 0;
+	      gbc.gridy = 1;
+	      gbc.weighty = 1;
+	      painelBotoes.add(botaoNome2Tag,gbc);
+	      painelDireita.add(painelBotoes,BorderLayout.EAST);
 	      
+	      Image imagemOriginal = null;
+		try {
+			imagemOriginal = ImageIO.read(new File("sample/outras-coisas.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	      Image imagem = getScaledImage(imagemOriginal, 150, 150);
+	      
+	      Box boxRadios = Box.createVerticalBox();
+	      ButtonGroup grupoRadios = new ButtonGroup();
+	      JRadioButton radioURL = new JRadioButton("pegar da URL",true);
+	      JRadioButton radioArquivo = new JRadioButton("pegar de um arquivo",false);
+	      grupoRadios.add(radioURL);
+	      grupoRadios.add(radioArquivo);
+	      boxRadios.add(radioURL);
+	      boxRadios.add(radioArquivo);
+	      
+	      JPanel painelImagem = new JPanel();
+	      painelImagem.setBorder(BorderFactory.createTitledBorder("Imagem"));
+	      painelImagem.setLayout(new BorderLayout());
+	      painelImagem.add(new JLabel(new ImageIcon(imagem)),BorderLayout.CENTER);
+	      painelImagem.add(boxRadios,BorderLayout.SOUTH);
+	      painelEsquerda.add(painelImagem,BorderLayout.WEST);
+	      
+	      final JPanel painelSelecaoImagemArquivo = new JPanel();
+	      painelSelecaoImagemArquivo.setBorder(BorderFactory.createTitledBorder("Selecione a imagem"));
+	      painelSelecaoImagemArquivo.setLayout(new GridBagLayout());
+	      JTextField textFieldSelecaoImagem = new JTextField(15);
+	      final JLabel labelSelecaoImagem = new JLabel("Informe a URL da imagem");
+	      final JButton botaoSelecaoImagem = new JButton("OK");
+	      gbc.gridx = 0;
+	      gbc.gridy = 0;
+	      gbc.weightx = 0;
+	      gbc.weighty = 0;
+	      gbc.anchor = GridBagConstraints.WEST;
+	      gbc.insets = new Insets(0, 0, 10, 5);
+	      painelSelecaoImagemArquivo.add(labelSelecaoImagem, gbc);
+	      gbc.gridx = 0;
+	      gbc.gridy = 1;
+	      gbc.insets = new Insets(0, 0, 50, 5);
+	      painelSelecaoImagemArquivo.add(textFieldSelecaoImagem, gbc);
+	      gbc.gridx = 1;
+	      gbc.gridy = 1;
+	      painelSelecaoImagemArquivo.add(botaoSelecaoImagem, gbc);
+	      painelDireita.add(painelSelecaoImagemArquivo, BorderLayout.CENTER);
+	      
+	      radioURL.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				labelSelecaoImagem.setText("Informe a URL da imagem");
+				botaoSelecaoImagem.setText("Ok");
+			}
+		});
+	      
+	      radioArquivo.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					labelSelecaoImagem.setText("Selecione o arquivo da imagem");
+					botaoSelecaoImagem.setText("Procurar");
+				}
+			});
+	    	  
 	      ArrayList<JLabel> labels = new ArrayList<JLabel>();
 	      ArrayList<JTextField> textFieldsNumero = new ArrayList<JTextField>();
 	      ArrayList<JTextField> textFieldsFaixas = new ArrayList<JTextField>();
 	      
-		for (int i = 0; i <1 ; i++) {
+		for (int i = 0; i < 20 ; i++) {
 
 			labels.add(new JLabel());
 			textFieldsNumero.add(new JTextField(3));
-			textFieldsFaixas.add(new JTextField(10));
+			textFieldsFaixas.add(new JTextField(15));
 
 			JLabel labelFaixa1 = labels.get(i);
 			labelFaixa1.setText("A Cor Do Som - Transe Total - 02 - Palco.mp3 --"+i);
@@ -180,6 +271,15 @@ public class TelaPrincipal extends JFrame {
 	    this.add(scrollPane, BorderLayout.CENTER);
 	    
 	    this.setVisible(true);
+	}
+
+	private Image getScaledImage(Image imagemOriginal, int largura, int altura) {
+		  BufferedImage resizedImg = new BufferedImage(largura, altura, BufferedImage.TYPE_INT_RGB);
+		    Graphics2D g2 = resizedImg.createGraphics();
+		    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		    g2.drawImage(imagemOriginal, 0, 0,largura, altura, null);
+		    g2.dispose();
+		    return resizedImg;
 	}
 	
 	
