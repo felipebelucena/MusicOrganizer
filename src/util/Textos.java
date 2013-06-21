1dbadd5562e84df91bdfa2ee16833b3e44e16ab0
Frank Junior <frankcbjunior@gmail.com>
2013-06-21 09:08:03 -0300

    adicionado modificadores final na classe de String

--- src/util/Textos.java
+++ src/util/Textos.java
@@ -4,43 +4,43 @@ public class Textos {
 	
 	public static String STRING_VAZIA = "";
 	
-	public static String TITULO = "Music Organizer";
-	public static String MENU_FILE = "File";
-	public static String MENU_ITEM_OPEN = "Open";
-	public static String LABEL_ARTISTA = "Artista";
-	public static String LABEL_ARLBUM = "Album";
-	public static String LABEL_ANO = "Ano";
-	public static String LABEL_GENERO = "Genero";
-	public static String BOTAO_SALVAR = "Salvar";
-	public static String BOTAO_NOME2TAG = "Nome > Tag";
-	public static String RADIOBUTTON_PEGAR_DA_URL = "pegar da URL";
-	public static String BORDA_MUSICAS = "Musicas";
-	public static String BORDA_IMAGEM = "Imagem";
-	public static String BORDA_SELECIONE_UM_IMAGEM = "Selecione a imagem";
-	public static String BORDA_TAGS_GERAIS = "Tags Gerais";
-	public static String OK = "ok";
-	public static String INFORME_URL_IMAGEM = "Informe a URL da imagem";
-	public static String SELECIONE_ARQUIVO_IMAGEM = "Selecione o arquivo da imagem";
-	public static String PROCURAR = "Procurar";
-	public static String POPUP_SELECIONE_UM_DISCO = "Selecione um disco primeiro";
-	public static String POPUP_DISCO_INVALIDO = "Selecione um disco válido";
-	public static String POPUP_SELECIONE_UMA_IMAGEM = "Selecione uma imagem JPG ou PNG";
-	public static String POPUP_ERRO = "Erro";
-	public static String POPUP_INFO = "Info";
-	public static String DIALOG_ESCOLHA_UM_CD = "Escolha um CD";
+	public static final String TITULO = "Music Organizer";
+	public static final String MENU_FILE = "File";
+	public static final String MENU_ITEM_OPEN = "Open";
+	public static final String LABEL_ARTISTA = "Artista";
+	public static final String LABEL_ARLBUM = "Album";
+	public static final String LABEL_ANO = "Ano";
+	public static final String LABEL_GENERO = "Genero";
+	public static final String BOTAO_SALVAR = "Salvar";
+	public static final String BOTAO_NOME2TAG = "Nome > Tag";
+	public static final String RADIOBUTTON_PEGAR_DA_URL = "pegar da URL";
+	public static final String BORDA_MUSICAS = "Musicas";
+	public static final String BORDA_IMAGEM = "Imagem";
+	public static final String BORDA_SELECIONE_UM_IMAGEM = "Selecione a imagem";
+	public static final String BORDA_TAGS_GERAIS = "Tags Gerais";
+	public static final String OK = "ok";
+	public static final String INFORME_URL_IMAGEM = "Informe a URL da imagem";
+	public static final String SELECIONE_ARQUIVO_IMAGEM = "Selecione o arquivo da imagem";
+	public static final String PROCURAR = "Procurar";
+	public static final String POPUP_SELECIONE_UM_DISCO = "Selecione um disco primeiro";
+	public static final String POPUP_DISCO_INVALIDO = "Selecione um disco válido";
+	public static final String POPUP_SELECIONE_UMA_IMAGEM = "Selecione uma imagem JPG ou PNG";
+	public static final String POPUP_ERRO = "Erro";
+	public static final String POPUP_INFO = "Info";
+	public static final String DIALOG_ESCOLHA_UM_CD = "Escolha um CD";
 	
 	//Erros
-	public static String ERRO_PROBLEMA_COM_A_INTERNET = "Problemas com a conexao com a internet";
-	public static String ERRO_URL_INVALIDA = "Selecione uma URL válida";
-	public static String ERRO_LISTA_NULA = "Lista nula";
-	public static String ERRO_LISTA_TAGS_VAZIA = "Lista de Tags Vazia";
+	public static final String ERRO_PROBLEMA_COM_A_INTERNET = "Problemas com a conexao com a internet";
+	public static final String ERRO_URL_INVALIDA = "Selecione uma URL válida";
+	public static final String ERRO_LISTA_NULA = "Lista nula";
+	public static final String ERRO_LISTA_TAGS_VAZIA = "Lista de Tags Vazia";
 	
 	//Arquivos
-	public static String IMAGEM_PADRAO = "imagem-padrao.jpg";
+	public static final String IMAGEM_PADRAO = "imagem-padrao.jpg";
 	
 	//formatos
-	public static String FORMATO_MP3 = "mp3";
-	public static String FORMATO_JPG = "jpg";
-	public static String FORMATO_PNG = "png";
+	public static final String FORMATO_MP3 = "mp3";
+	public static final String FORMATO_JPG = "jpg";
+	public static final String FORMATO_PNG = "png";
 
 }
